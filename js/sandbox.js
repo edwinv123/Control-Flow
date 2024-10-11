32b7af664d8be3e32d5e73c8bbed47a69fc9533d
edwinv123 <126evalentinnunez@frhsd.com>
2024-10-11 14:00:34 -0400

    first commit

---
+++ js/sandbox.js
@@ -0,0 +1,41 @@
+// for loops
+
+// for(let i = 0; i < 5; i++){
+//    console.log('in loop:', i);
+// }
+
+// console.log('loop finished');
+
+// const names = ['shaun', 'mario', 'luigi'];
+
+// for(let i = 0; o < names.length; i++){
+//    // console.log(names[i]);
+//    let html = '<div>${names[i]}</div>';
+//    console.log(html);   
+// }
+
+// let i = 3;
+
+// do{
+//     console.log('val of i is: ', i);
+//     i++
+// }while(i < 5);
+
+// if statements
+// const age = 20;
+
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 4){
+//     console.log("that's a lot of ninjas");
+// }
+
+const password = 'pass';
+
+if(password.length >= 8){
+    console.log('that password is long enough!');
+}
